525fecb4bfa9bcf967803bab1cd53861ccdb3414
Forsyth-Creations <robert.h.forsyth@gmail.com>
2023-08-05 22:13:03 -0400

    better formatting

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -9,14 +9,22 @@ import CircularProgress from '@mui/material/CircularProgress';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import LoginRedirect from '@/comps/LoginRedirect';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
 
 export default function MyPageComponent() {
 
   return (
     <main>
       <LoginRedirect redirectUrl="/console">
-        <Container>
-          <Typography>Loading Health Monitor<CircularProgress /></Typography>
+        <Container sx={{minHeight:"100vh"}}>
+          <Stack sx={{display: "flex", 
+          justifyContent: "center", 
+          alignItems: "center",
+          paddingTop : "12vh"}}>
+          <Typography>Loading Health Monitor</Typography>
+          <CircularProgress />
+          </Stack>
         </Container>
       </LoginRedirect>
     </main>
